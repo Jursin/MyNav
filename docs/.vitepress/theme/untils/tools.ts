b340e10dc0dcc4dcf298fedca61ba3355a28dfea
Jursin <1836059252@qq.com>
2025-08-06 17:45:19 +0800

    doc: 添加实用部分工具

--- docs/.vitepress/theme/untils/tools.ts
+++ docs/.vitepress/theme/untils/tools.ts
@@ -55,6 +55,125 @@ export const NAV_DATA: NavData[] = [
     ],
   },
   {
+    title: '日常工具',
+    items: [
+      {
+        icon: 'https://www.everauto.net/images/App32.png',
+        title: 'zTasker',
+        desc: '一款定时任务软件',
+        link: 'https://www.everauto.net/'
+      },
+      {
+        icon: 'https://avatars.githubusercontent.com/u/89929298?s=200&v=4',
+        title: 'AutoDarkMode',
+        desc: '在预定时间切换 Windows 的暗色和亮色主题',
+        link: 'https://github.com/AutoDarkMode/Windows-Auto-Night-Mode'
+      },
+      {
+        icon: 'https://www.voidtools.com/favicon.ico',
+        title: 'Everything',
+        desc: '基于名称快速定位文件和文件夹',
+        link: 'https://www.voidtools.com/zh-cn/'
+      },
+      {
+        icon: 'https://www.filelocator.cn/wp-content/uploads/2020/12/cropped-20160425040730972-32x32.png',
+        title: 'File Locator Pro',
+        desc: '一款高效的文件搜索软件',
+        link: 'https://www.filelocator.cn/'
+      },
+      {
+        icon: 'https://i-cdn.apsgo.com/cdn/rFguxK8DSDzZJmLWemOypdpG8bNJJPadTTYwCllW.png',
+        title: 'ReNamer',
+        desc: '一款高效的文件重命名软件',
+        link: 'https://www.den4b.com/products/renamer'
+      },
+      {
+        icon: 'https://gitee.com/ilgnefz/once_power/raw/master/assets/images/logo.png',
+        title: 'Once Power',
+        desc: '基于 Flutter 开发的文件批量重命名、批量移动删除工具',
+        link: 'https://github.com/ilgnefz/once_power'
+      },
+      {
+        icon: 'https://fastcopy.jp/img/fastcopy64-2.png',
+        title: 'FastCopy',
+        desc: 'Windows上最快的复制/备份软件',
+        link: 'https://fastcopy.jp/'
+      },
+      {
+        icon: '/Textify.ico',
+        title: 'Textify',
+        desc: '从不提供此类功能的对话框中复制一些文本',
+        link: 'https://ramensoftware.com/textify'
+      },
+      {
+        icon: 'https://raw.githubusercontent.com/QL-Win/QuickLook/refs/heads/master/QuickLook/Resources/app.svg',
+        title: 'QuickLook',
+        desc: '将 MacOS “快速查看”功能引入 Windows',
+        link: 'https://github.com/QL-Win/QuickLook'
+      },
+      {
+        icon: 'https://ts1.tc.mm.bing.net/th/id/OSAAS.61465FDED8BC78170F7B4F9D07858BA7?w=72&h=72&c=17&rs=1&o=6&cb=thws4&dpr=1.5&pid=TechQna',
+        title: 'Internet Download Manager',
+        desc: '一个下载加速器',
+        link: 'https://www.internetdownloadmanager.com/'
+      },
+      {
+        icon: 'https://raw.githubusercontent.com/BluePointLilac/ContextMenuManager/master/ContextMenuManager/Properties/AppIcon.ico',
+        title: 'ContextMenuManager',
+        desc: '一个纯粹的Windows右键菜单管理程序',
+        link: 'https://bluepointlilac.github.io/ContextMenuManager/'
+      },
+      {
+        icon: '/FluentContextMenu.ico',
+        title: 'FluentContextMenu',
+        desc: '右键菜单美化',
+        link: 'https://winmoes.com/tools/17626.html'
+      },
+      {
+        icon: 'https://translucenttb.com/wp-content/uploads/2023/12/translucenttb-2-150x150.png',
+        title: 'Translucent​TB',
+        desc: '使Windows任务栏半透明/透明的轻量级实用程序',
+        link: 'https://translucenttb.com/'
+      },
+      {
+        icon: 'https://github.com/AlexanderPro/SmartSystemMenu/raw/master/SmartSystemMenu/Images/SmartSystemMenuLogo.png',
+        title: 'SmartSystemMenu',
+        desc: '扩展系统中所有窗口的系统菜单',
+        link: 'https://github.com/AlexanderPro/SmartSystemMenu'
+      },
+      {
+        icon: 'https://www.cpuid.com/medias/images/softwares/cpu-z.svg',
+        title: 'CPU-Z',
+        desc: '显示处理器相关信息',
+        link: 'https://www.cpuid.com/softwares/cpu-z.html'
+      },
+      {
+        icon: 'https://crystalmark.info/favicon.ico',
+        title: 'CrystalDiskInfo',
+        desc: '查看磁盘详细信息',
+        link: 'https://crystalmark.info/en/software/crystaldiskinfo/'
+      },
+      {
+        icon: '/CrystalDiskMark.ico',
+        title: 'CrystalDiskMark',
+        desc: '一个简单的磁盘基准测试软件',
+        link: 'https://crystalmark.info/en/software/crystaldiskmark/'
+      },
+      {
+        icon: 'https://geekuninstaller.com/assets/images/icon_geek_32@2x.png',
+        title: 'Geek Uninstaller',
+        desc: '好用的卸载软件',
+        link: 'https://geekuninstaller.com/'
+      },
+      {
+        icon: 'https://www.bcuninstaller.com/assets/favicon.ico',
+        title: 'BCUninstaller',
+        desc: '批量垃圾卸载程序',
+        link: 'https://www.bcuninstaller.com/'
+      },
+    ],
+  },
+  {
     title: 'IDE',
     items: [
       {
@@ -76,5 +195,54 @@ export const NAV_DATA: NavData[] = [
         link: 'https://www.jetbrains.com/zh-cn/pycharm/'
       },
     ],
+  },
+  {
+    title: '文档站搭建',
+    items: [
+      {
+        icon: 'https://vitepress.dev/vitepress-logo-large.svg',
+        title: 'VitePress',
+        desc: '由 Vite 和 Vue 驱动的静态站点生成器',
+        link: 'https://vitepress.dev/zh/'
+      },
+      {
+        icon: 'https://vitepress.yiov.top/logo.png',
+        title: 'VitePress',
+        badge: {
+          text: '🔥 非常推荐',
+          type: 'danger'
+        },
+        desc: '快速上手中文教程，含更多美化教程',
+        link: 'https://vitepress.yiov.top/'
+      },
+      {
+        icon: 'https://vuepress.vuejs.org/images/hero.png',
+        title: 'VuePress',
+        desc: 'Vue 驱动的静态网站生成器',
+        link: 'https://vuepress.vuejs.org/zh/'
+      },
+      {
+        icon: 'https://theme-hope-assets.vuejs.press/logo.svg',
+        title: 'VuePress Theme Hope',
+        badge: {
+          text: '✨ 功能丰富',
+          type: 'tip'
+        },
+        desc: '一个具有强大功能的 vuepress 主题✨',
+        link: 'https://theme-hope.vuejs.press/zh/'
+      },
+      {
+        icon: 'https://astro.build/favicon.svg',
+        title: 'Astro',
+        desc: '一个内容驱动的网站的网络框架',
+        link: 'https://astro.build/'
+      },
+      {
+        icon: 'https://astro-pure.js.org/_image?href=%2F_astro%2Favatar.g4D3QZBA.png&w=1370&h=1370&f=webp',
+        title: 'Astro Theme Pure',
+        desc: '一个简洁的 Astro 主题',
+        link: 'https://astro-pure.js.org/'
+      }
+    ]
   }
 ]
